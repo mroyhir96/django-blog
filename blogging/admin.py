fdb1852db21bf02ba87964964e2038d16e2d1f6e
Mike Hirano <mhir86@uw.edu>
2023-12-11 20:12:53 -0800

    applied black

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
 class CategoryInline(admin.TabularInline):
     model = Category.posts.through
     extra = 1
 
+
 class PostAdmin(admin.ModelAdmin):
     inlines = [CategoryInline]
 
+
 admin.site.register(Post, PostAdmin)
 
-class CategoryAdmin(admin.ModelAdmin):
-    exclude = ('posts',)
 
-admin.site.register(Category,CategoryAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ("posts",)
+
+
+admin.site.register(Category, CategoryAdmin)
--- blogging/views.py
+++ blogging/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views import View
-#from django.views.generic import ListView, DetailView
+
+# from django.views.generic import ListView, DetailView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from blogging.models import Post
 
+
 class PostListView(ListView):
     model = Post
-    template_name = 'blogging/list.html'
-    context_object_name = 'posts'
-    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = "blogging/list.html"
+    context_object_name = "posts"
+    queryset = Post.objects.exclude(published_date__exact=None).order_by(
+        "-published_date"
+    )
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blogging/detail.html'
+    template_name = "blogging/detail.html"
     queryset = Post.objects.exclude(published_date__exact=None)
