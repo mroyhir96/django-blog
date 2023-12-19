e9a6586cf6ee63b4a5e279ca1720f635cc11fb09
Mike Hirano <mhir86@uw.edu>
2023-12-18 16:18:29 -0800

    mod for dj-static

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
 from blogging.views import PostListView, PostDetailView
-#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
 ]
-
-#urlpatterns += staticfiles_urlpatterns()
-
-""" urlpatterns += urlpatterns('',
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT,
-    }),
- ) """
