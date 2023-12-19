bcfdeb63b4e35d447f1f31b16a066b27e8380c0b
Mike Hirano <mhir86@uw.edu>
2023-12-18 17:15:07 -0800

    added STATIC DIRS envir var

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-STATIC_ROOT = "staticfiles"
-STATIC_URL = "/static/"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,6 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"),)
 
 STATIC_URL = "/static/"
 
