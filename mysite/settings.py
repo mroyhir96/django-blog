a153c4bf8e1fb0804c35fe86c1a715d9d7034df6
Mike Hirano <mhir86@uw.edu>
2023-12-22 21:28:35 -0800

    delete commented lines

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,12 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
-    'django.contrib.sites', # new
-    'allauth', # new
-    'allauth.account', # new
-    'allauth.socialaccount', # new
-    'allauth.socialaccount.providers.github', # new   
+    "django.contrib.sites",  # new
+    "allauth",  # new
+    "allauth.account",  # new
+    "allauth.socialaccount",  # new
+    "allauth.socialaccount.providers.github",  # new
     "polling",
     "blogging",
 ]
@@ -140,4 +139,4 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = "none"
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,13 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
-#from .views import Home # new
 urlpatterns = [
     path("", include("blogging.urls")),  # <-- add this
     path("polling/", include("polling.urls")),  # <-- Add this
     path("admin/", admin.site.urls),
-    path('accounts/', include('allauth.urls')), # new   
-    #path('', Home.as_view(), name='home'), # new
+    path("accounts/", include("allauth.urls")),  # new
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
